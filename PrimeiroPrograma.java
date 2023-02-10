7b62de59ffac644b546c008cd7b1f4f72bc2133b
Leticia Cristina Santos <leticiaportalestrelas@gmail.com>
2023-02-10 12:19:16 -0300

    Add files via upload
    First Commit.

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,35 @@
+package com.dio;
+
+import java.util.Scanner;
+
+public class PrimeiroPrograma {
+
+
+public static void main(String[]args) 
+
+{
+	
+     String nome = "Leticia";
+     double idade = 36;
+      
+      System.out.println("É um prazer tê-lo por aqui. Por favor digite seu nome:");
+      
+          Scanner input = new Scanner (System.in);
+          
+          nome = input.next(); 
+          
+          System.out.println("Leticia, qual é a sua idade?"); 
+          
+          idade = input.nextDouble(); 
+          
+          System.out.println("Obrigado por responder ao nosso Quiz!"); 
+                   
+}}
+          
+          
+    
+      
+      
+
+	
+        
