7b4a19447bf431c2f54e4a755501d510053d0a81
Leticia Santos <leticiaportalestrelas@gmail.com>
2023-02-13 14:44:48 -0300

    primeiroprogramaintelliej

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+public class main {
+       public static void main(String[] args)
+{
+        System.out.println("Hello world!");
+
+        System.out.println("Me chamo Leticia, mas pode me chamar de Lê");
+
+
+    }}
